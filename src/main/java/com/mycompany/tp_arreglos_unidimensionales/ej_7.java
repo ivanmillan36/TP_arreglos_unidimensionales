03f18e1611dfd2738b125a6e1b2a6eebff1ee530
ivan millan <ivanmillan36@gmail.comgit>
2020-09-29 16:17:07 -0300

    fin ej 7

---
+++ src/main/java/com/mycompany/tp_arreglos_unidimensionales/ej_7.java
@@ -0,0 +1,54 @@
+/*
+7.Escriba una aplicación que solicite  y cargue en un arreglo 10  dígitos enteros, 
+luego cree dos nuevos arreglos y asigne al primero los números ingresados por el usuario de formaa ascendente y 
+en el segundo de forma descendente.Muestre los 2 arreglos por pantalla.
+ */
+package com.mycompany.tp_arreglos_unidimensionales;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ivanmillan36
+ */
+public class ej_7 {
+    public static void main(String[] args) {
+        int arreglo[] = new int[10];
+        int arregloAscendente[] = new int[10];
+        int arregloDescendente[] = new int[10];
+        
+        cargarArreglo(arreglo);
+        System.out.println("Arreglo ingresado: " + Arrays.toString(arreglo));
+        
+        Arrays.sort(arreglo);
+        arregloAscendente = arreglo;
+        arregloDescendente = invertirArreglo(arregloAscendente);
+        
+        System.out.println("Arreglo ascendente: " + Arrays.toString(arregloAscendente));
+        System.out.println("Arreglo descendente: " + Arrays.toString(arregloDescendente));
+        
+    }
+    
+    public static void cargarArreglo(int arreglo[]){
+        Scanner sc = new Scanner(System.in);
+        
+        for(int i=0; i< arreglo.length; i++){
+            System.out.println("Ingrese un entero para la posicion " + (i+1) + " del arreglo: ");
+            arreglo[i] = sc.nextInt();
+        }
+    }
+    
+    public static int [] invertirArreglo (int arreglo[]){
+        
+        int nuevoArreglo[] = new int[arreglo.length];
+        int cont = 0;
+        
+        for(int i=(arreglo.length -1) ; i >=0 ; i--){
+            nuevoArreglo[cont] = arreglo[i];
+            cont++;
+        }
+        return nuevoArreglo;
+    }
+    
+}
